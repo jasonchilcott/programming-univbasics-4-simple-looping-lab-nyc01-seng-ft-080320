9752c7a2a1e50ae821089bec5a3b5a713ffacd31
Jason Chilcott <jasonchilcott@me.com>
2020-07-13 23:47:54 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,3 +15,11 @@ def loop_message_n_times(message, times)
     counter += 1
   end
 end
+
+def output_array(array)
+  counter = 0
+  while array[counter] do
+    puts array[counter]
+    counter += 1
+  end
+end
